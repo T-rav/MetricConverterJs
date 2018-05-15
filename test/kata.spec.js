2c619c79bffb7fff8d6dbd2af8735c201752d8a4
Travis Frisinger <T-rav@users.noreply.github.com>
2018-05-15 09:34:40 +0200

    Added factory for Converter

--- test/kata.spec.js
+++ test/kata.spec.js
@@ -2,7 +2,7 @@ describe("Converter", function () {
 	describe("Kilometers to Miles", function(){
 		it("When 1 kilometer should return 0.621371 miles", function() {
 			// arrange
-			var converter = new Converter();
+			var converter = MakeConverter();
 			var input = 1;
 			// act
 			var actual = converter.kilometersToMiles(input);
@@ -13,7 +13,7 @@ describe("Converter", function () {
 
 		it("When 1.5 kilometer should return 0.932057 miles", function() {
 			// arrange
-			var converter = new Converter();
+			var converter = MakeConverter();
 			var input = 1.5;
 			// act
 			var actual = converter.kilometersToMiles(input);
@@ -26,11 +26,11 @@ describe("Converter", function () {
 	describe("Celsius to Fahrenheit", function(){
 		describe("Given whole numbers to convert", function(){
 			[{c:0,f:32},
-				{c:100,f:212}
-			].forEach(function(element){
+			{c:100,f:212}]
+			.forEach(function(element){
 				it("When " +element.c + " C should return " + element.f + " F", function() {
 					// arrange
-					var converter = new Converter();
+					var converter = MakeConverter();
 					var input = element.c;
 					// act
 					var actual = converter.celsiusToFahrenheit(input);
@@ -43,11 +43,11 @@ describe("Converter", function () {
 
 		describe("Given fractional numbers to convert", function(){
 			[{c:0.5,f:32.9},
-			 {c:22.3,f:72.14}
-			].forEach(function(element){
+			 {c:22.3,f:72.14}]
+			.forEach(function(element){
 				it("When " +element.c + " C should return " + element.f + " F", function() {
 					// arrange
-					var converter = new Converter();
+					var converter = MakeConverter();
 					var input = element.c;
 					// act
 					var actual = converter.celsiusToFahrenheit(input);
@@ -63,11 +63,11 @@ describe("Converter", function () {
 		describe("Given whole numbers to convert", function(){
 			[{k:0, p:0},
 			 {k:10,p:22.04622622},
-			 {k:2, p:4.40924524}
-			].forEach(function(element){
+			 {k:2, p:4.40924524}]
+			.forEach(function(element){
 				it("When " +element.k + " kilogram should return " + element.p + " pounds", function() {
 					// arrange
-					var converter = new Converter();
+					var converter = MakeConverter();
 					var input = element.k;
 					// act
 					var actual = converter.kilogramsToPounds(input);
@@ -81,11 +81,11 @@ describe("Converter", function () {
 		describe("Given fractional numbers to convert", function(){
 			[{k:0.5, p:1.10231131},
 			 {k:2.205,p:4.86119288},
-			 {k:9.91, p:21.84781018}
-			].forEach(function(element){
+			 {k:9.91, p:21.84781018}]
+			.forEach(function(element){
 				it("When " +element.k + " kilogram should return " + element.p + " pounds", function() {
 					// arrange
-					var converter = new Converter();
+					var converter = MakeConverter();
 					var input = element.k;
 					// act
 					var actual = converter.kilogramsToPounds(input);
@@ -102,11 +102,11 @@ describe("Converter", function () {
 			describe("while converting to US gallons", function(){
 				[{l:0, g:0},
 				 {l:10,g:2.641720524},
-				 {l:2, g:0.528344105}
-				].forEach(function(element){
+				 {l:2, g:0.528344105}]
+				.forEach(function(element){
 					it("When " +element.l + " liters should return " + element.g + " gallons", function() {
 						// arrange
-						var converter = new Converter();
+						var converter = MakeConverter();
 						var input = element.l;
 						var units = "US";
 						// act
@@ -121,11 +121,11 @@ describe("Converter", function () {
 			describe("while converting to UK gallons", function(){
 				[{l:0, g:0},
 				 {l:10,g:2.199692483},
-				 {l:2, g:0.439938497}
-				].forEach(function(element){
+				 {l:2, g:0.439938497}]
+				.forEach(function(element){
 					it("When " +element.l + " liters should return " + element.g + " gallons", function() {
 						// arrange
-						var converter = new Converter();
+						var converter = MakeConverter();
 						var input = element.l;
 						var units = "UK";
 						// act
@@ -143,11 +143,11 @@ describe("Converter", function () {
 		describe("while converting to US gallons", function(){
 			[{l:0.5, g:0.132086026},
 			 {l:2.34,g:0.618162603},
-			 {l:7.29, g:1.925814262}
-			].forEach(function(element){
+			 {l:7.29, g:1.925814262}]
+			.forEach(function(element){
 				it("When " +element.l + " liters should return " + element.g + " gallons", function() {
 					// arrange
-					var converter = new Converter();
+					var converter = MakeConverter();
 					var input = element.l;
 					var units = "US";
 					// act
@@ -162,11 +162,11 @@ describe("Converter", function () {
 		describe("while converting to UK gallons", function(){
 			[{l:0.5, g:0.109984624},
 			 {l:2.34,g:0.514728041},
-			 {l:7.29, g:1.60357582}
-			].forEach(function(element){
+			 {l:7.29, g:1.60357582}]
+			 .forEach(function(element){
 				it("When " +element.l + " liters should return " + element.g + " gallons", function() {
 					// arrange
-					var converter = new Converter();
+					var converter = MakeConverter();
 					var input = element.l;
 					var units = "UK";
 					// act
@@ -179,3 +179,7 @@ describe("Converter", function () {
 		});
 	});
 });
+
+function MakeConverter(){
+	return new Converter();
+}
